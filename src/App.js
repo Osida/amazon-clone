a5eca8b4c2e6d5ac8e4b5478d951419cfdb98e54
osida <you@example.com>
2020-12-23 12:20:22 -0500

    set up intital react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    // BEM convention
+    <div className="app">
+      <h1>Whatz Up from App.js</h1>
+      {/* Header -> Top banner*/}
+      {/* Home */}
+    </div>
+  );
+}
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfKbn2QZ6Wnzcz8EB-WCVjQcTCM1Khki4",
+  authDomain: "clone-a13f4.firebaseapp.com",
+  projectId: "clone-a13f4",
+  storageBucket: "clone-a13f4.appspot.com",
+  messagingSenderId: "947956826129",
+  appId: "1:947956826129:web:134228f7db407c830c31b0",
+};
